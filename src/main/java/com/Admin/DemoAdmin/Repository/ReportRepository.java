3debd63c438b9c500856d0986568cdbfdc2aaf64
DAO <daohdde170519@fpt.edu.vn>
2024-06-04 23:35:21 +0700

    Report(Thiếu controller)

--- src/main/java/com/Admin/DemoAdmin/Repository/ReportRepository.java
+++ src/main/java/com/Admin/DemoAdmin/Repository/ReportRepository.java
@@ -5,7 +5,10 @@
 package com.Admin.DemoAdmin.Repository;
 
 import com.Admin.DemoAdmin.Entity.Report;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,5 +16,8 @@ import org.springframework.stereotype.Repository;
  * @author DAO
  */
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Integer> {}
+public interface ReportRepository extends JpaRepository<Report, Integer> {
+    @Query("select r from Report r where r.reportId LIKE %?1% or c.description LIKE %?1%  or c.reason LIKE %?1% or c.createDate LIKE %?1% or c.reportUser LIKE %?1% or c.reportedUser LIKE %?1%")
+    Page<Report> searchCategories(String keyword, Pageable pageable);
+}
 
---
+++ src/main/java/com/Admin/DemoAdmin/Service/Impl/ReportServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Admin.DemoAdmin.Service.Impl;
+
+import com.Admin.DemoAdmin.Entity.Report;
+import com.Admin.DemoAdmin.Repository.ReportRepository;
+import com.Admin.DemoAdmin.Service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author DAO
+ */
+@Service
+public class ReportServiceImpl implements ReportService{
+    @Autowired
+    private ReportRepository reportRepository;
+    @Override
+    public Page<Report> findPaginated(Pageable pageable) {
+      return  reportRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Report> searchCategories(String keyword, Pageable pageable) {
+        return  reportRepository.searchCategories(keyword, pageable);
+    }
+    
+}
